e7500b7cc13642abe9c277b9bb5a12205222e243
JavierFueyo <157507995+JavierFueyo@users.noreply.github.com>
2024-02-21 10:27:28 +0100

    Managers reorganizados
    Ahora están en la carpeta "Scripts/Managers"

---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] private InputActionAsset _playerActions;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
