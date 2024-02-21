4cb7a0e894569217694715daad7085c3f73f19c7
Rubens <149378012+RubensDieWeise@users.noreply.github.com>
2024-02-21 22:17:32 +0100

    Instancia implementada en el GameManager
    Instancia única para que todo el mundo pueda acceder a él y singleton para que no esté duplicado. Ya de paso he organizado el código en regiones.

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -4,12 +4,47 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public int ObjetosTotales { get { return objetosTotales; } }
+    #region properties
+    
+
+    static private GameManager _instance;
+    
+
+    static public GameManager Instance // Todos podéis usar este método (escrito: GameManager.Instance) para acceder al GameManager y a cualquiera de sus métodos
+    {
+        get { return _instance; }
+    }
+
     private int objetosTotales;
+
+    public int ObjetosTotales { get { return objetosTotales; } }
+
+    #endregion
+    #region methods
+
+    private void Awake()
+    {
+        if (_instance == null) _instance = this; // Sólo queremos un GameManager, así que usamos el patrón singleton
+        else Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        
+    }
 
     public void SumarObjetos(int objetosAsumar)
     {
         objetosTotales += objetosAsumar;
         Debug.Log(objetosTotales);
     }
+
+    #endregion
+}
+
+
+
+
+
+    
 }
