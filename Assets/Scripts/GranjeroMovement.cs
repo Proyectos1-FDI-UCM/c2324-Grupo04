66edcf449d80152c7c2d2ef3ca2c63851e4a82ac
Dpgo4 <danipont@ucm.es>
2024-02-22 17:54:09 +0100

    movimiento
    Movimiento horizontal

--- Assets/Scripts/GranjeroMovement.cs
+++ Assets/Scripts/GranjeroMovement.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GranjeroMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector2 movement;
+    private Rigidbody2D rb;
+    private float Horizontal;
+
+    [SerializeField] private int speed = 3;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+    }
+   
+    private void OnMovement (InputValue value) 
+    {
+        movement = value.Get<Vector2>();
+    }
+   
+    private void FixedUpdate ()
+    {
+        //Variante 1
+       rb.MovePosition(rb.position + movement * speed * Time.deltaTime); //Sin aceleracion
+       
+        //variante 2 con aceleracion1   (se puede cambiar el linear drag)
+        /*
+        if (movement.x != 0){
+            rb.velocity = movement * speed;
+        }
+        */
+
+        //variante 3 con aceleracion2
+        /*
+         rb.AddForce(movement * speed);
+         * */
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -47,4 +47,4 @@ public class GameManager : MonoBehaviour
 
 
     
-}
+
