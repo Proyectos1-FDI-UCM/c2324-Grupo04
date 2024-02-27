87691cb034bccae25ddc652465814ff3b292b0c5
Rubens <149378012+RubensDieWeise@users.noreply.github.com>
2024-02-27 23:19:51 +0100

    Componente genérico de vida
    Que tiene vida máxima y un método público para alterar la vida que tiene.

--- Assets/Scripts/Genericos/HealthComponent.cs
+++ Assets/Scripts/Genericos/HealthComponent.cs
@@ -4,6 +4,24 @@ using UnityEngine;
 
 public class HealthComponent : MonoBehaviour
 {
+    [SerializeField] int _hitPoints;
+    [SerializeField] int _maxHitPoints;
+
+    public void AddLife(int addition)
+    {
+        _hitPoints += addition;
+        if (_hitPoints > _maxHitPoints)
+        {
+            _hitPoints = _maxHitPoints;
+        }
+        else if (_hitPoints < 0)
+        {
+            // Llamada a posible animación de muerte
+            Destroy(this.GameObject); // Habría que ver cómo actuar en el caso de que sea el jugador el que muere
+        }
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
