bfd8bb76d8de39c186998b0b8fee6a91890cb579
RubensDieWeise <149378012+RubensDieWeise@users.noreply.github.com>
2024-02-28 10:29:03 +0100

    Escena mía cambiada

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -49,6 +49,7 @@ public class GameManager : MonoBehaviour
 
     private void ActivaHorca()
     {
+        Debug.Log("Horca activada");
         //_playersHorcaAttack.SetActive(true);
         _playersHorcaAttack.enabled = true;
     }
