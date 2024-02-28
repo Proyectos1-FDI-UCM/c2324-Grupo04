df07172db2c6d732ea2e52bd9f035ddccc7c45c5
Bucerillo <antobuce@ucm.es>
2024-02-28 11:00:45 +0100

    Camara Actualizada

--- Assets/Scripts/Camera/Camera Movement.cs
+++ Assets/Scripts/Camera/Camera Movement.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     [SerializeField] private float followSpeed = 2f;
-    [SerializeField] private float xOffset = 1f;
+    [SerializeField] private float xOffset = 4f;
     [SerializeField] private float yOffset = 1f;
     [SerializeField] private Transform target;
-    private float zPos = -10f;
+    private float yOffsetStartx1 = 73f;
+	  private float yOffsetStarty1 = -3.5f;
+	  private float yOffsetEnd1 = 93f;
+	  private float zPos = -10f;
+    private float Camerax1=84.4f;
+	  private float Cameray1 = -9f;
 
 
-    // Start is called before the first frame update
-    void Start()
+	// Start is called before the first frame update
+	void Start()
     {
 
     }
@@ -20,7 +25,17 @@ public class CameraMovement : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        Vector3 newpos = new Vector3(target.position.x + xOffset + xOffset, target.position.y + yOffset, zPos);
-        transform.position = Vector3.Slerp(transform.position, newpos, followSpeed * Time.deltaTime);
+        if (target.position.x>yOffsetStartx1 && target.position.y<yOffsetStarty1 && target.position.x<yOffsetEnd1)
+        {
+			    Vector3 newpos = new Vector3(Camerax1,Cameray1, zPos);
+			    transform.position = Vector3.Slerp(transform.position, newpos, followSpeed * Time.deltaTime);
+		    }
+        else
+        {
+			    Vector3 newpos = new Vector3(target.position.x + xOffset, yOffset, zPos);
+			    transform.position = Vector3.Slerp(transform.position, newpos, followSpeed * Time.deltaTime);
+		    }
+        
+    
     }
 }
