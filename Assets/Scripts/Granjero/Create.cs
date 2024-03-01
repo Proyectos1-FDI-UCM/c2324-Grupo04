4f8537a78cdecb8683e29fb06e2236003d47761a
Dpgo4 <danipont@ucm.es>
2024-03-01 12:10:43 +0100

    Enemigos prefab
    Añadido prefab de enemigos y hacen daño

--- Assets/Scripts/Granjero/Create.cs
+++ Assets/Scripts/Granjero/Create.cs
@@ -12,14 +12,19 @@ public class Create : MonoBehaviour
     private float _tacoste = 0;
     [SerializeField]
     private float _secoste = 0;
+
+
     private void OnAction2()
     {
-       // if (GameManager.Instance.ObtenerCuerdas() >= _tacoste)
-       // {
-            GameObject trampolin = Instantiate(Trampoline, _myTransform.position, Quaternion.identity);
+
+        if (GameManager.Instance.ObtenerCuerdas() >= _tacoste)
+        {
+            GameObject trampolin = Instantiate(Trampoline, _myTransform.position , Quaternion.identity);
             print(trampolin);
-       // }
+            Debug.Log("Trampolin");
+        }
     }
+
     private void Start()
     {
         _myTransform = transform;
