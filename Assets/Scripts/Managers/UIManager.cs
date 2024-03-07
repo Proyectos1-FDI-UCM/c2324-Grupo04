23c9a8a833c6c0ee0bdd40e85ce6ba768b2c924c
Rubens <149378012+RubensDieWeise@users.noreply.github.com>
2024-03-07 23:38:40 +0100

    Primer menú de pausa
    Se para el juego y pone "PAUSA" en la pantalla

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    #region references
+
+    [SerializeField] GameObject _menuDePausa;
+    #endregion 
+
     private bool _paused = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _menuDePausa.SetActive(false);
     }
 
     // Update is called once per frame
@@ -23,12 +28,14 @@ public class UIManager : MonoBehaviour
         if (_paused)
         {
             Debug.Log("Salida de pausa");
+            _menuDePausa.SetActive(false);
             Time.timeScale = 1.0f;
             _paused = false;
         }
         else
         {
             Debug.Log("PAUSA");
+            _menuDePausa.SetActive(true);
             Time.timeScale = 0.0f;
             _paused = true;
         }
