81903e54dcc9da26d93e554fd0afc1c08d2444c0
Rubens <149378012+RubensDieWeise@users.noreply.github.com>
2024-03-07 23:29:56 +0100

    Sigo con la escalera
    Lo dejo así porque tenemos asuntos más imperiosos, pero compila perfectamente todo.

--- Assets/StairComponent.cs
+++ Assets/StairComponent.cs
@@ -10,12 +10,14 @@ public class StairComponent : MonoBehaviour
     private Transform _myTransform;
     [SerializeField] private Transform _endTransform;
     private Transform _playerTransform;
+    private BoxCollider2D _myCollider;
 
     #endregion
 
     #region variables 
 
-    private float _maxDistance = 0.2f;
+    [SerializeField] private float _maxDistance = 1f;
+    [SerializeField] private float _exitDistance = 10f;
     [SerializeField] private float _lerpFactor = 1f;
 
     #endregion
@@ -26,34 +28,52 @@ public class StairComponent : MonoBehaviour
 
     #endregion
 
-    //#region methods
+    #region methods
 
-    //private void OnGoDown()
-    //{
-    //    if (!_goingDown &&  Mathf.Abs((_myTransform.position - _playerTransform.position).magnitude) <= _maxDistance)
-    //    {
-    //        _playerTransform.position = _myTransform.position;
-    //        _goingDown = true; Debug.Log("_goingDown: " + _goingDown);
-    //    }
-    //}
+    private void OnGoDown()
+    {
+        if (!_goingDown && Mathf.Abs((_myTransform.position - _playerTransform.position).magnitude) <= _maxDistance)
+        {
+            _playerTransform.position = _myTransform.position;
+            _goingDown = true; Debug.Log("_goingDown: " + _goingDown);
+            _myCollider.enabled = false;
+        }
+        if ( _goingDown )
+        {
+            Debug.Log("Ya estás bajando");
+        }
+        if (Mathf.Abs((_myTransform.position - _playerTransform.position).magnitude) > _maxDistance)
+        {
+            Debug.Log("Estás muy lejos");
+        }
+    }
 
-    //#endregion
+    #endregion
 
 
 
-    //// Start is called before the first frame update
-    //void Start()
-    //{
-    //    _myTransform = transform;
-    //    _playerTransform = GameManager.Instance.ReferenciaTransformGranjero();
-    //}
+    // Start is called before the first frame update
+    void Start()
+    {
+        _myTransform = transform;
+        _playerTransform = GameManager.Instance.ReferenciaTransformGranjero();
+        _myCollider = GetComponent<BoxCollider2D>();
+    }
 
-    //// Update is called once per frame
-    //void Update()
-    //{
-    //    if (_goingDown)
-    //    {
-    //        _playerTransform.position = Vector3.Lerp(_playerTransform.position, _endTransform.position, _lerpFactor);
-    //    }
-    //}
+    // Update is called once per frame
+    void Update()
+    {
+        if (_goingDown)
+        {
+            if (Mathf.Abs((_myTransform.position - (_myTransform.position + _endTransform.position)).magnitude) <= _maxDistance)
+            {
+                _goingDown = false;
+                Debug.Log("Has llegado al fin de las escaleras");
+            }
+            else
+            {
+                _playerTransform.position = Vector3.Lerp(_playerTransform.position, _endTransform.position, Time.deltaTime * _lerpFactor);
+            }
+        }
+    }
 }
