f79dd720a0955922f63612ba4ce8ae3e678450da
Rubens <149378012+RubensDieWeise@users.noreply.github.com>
2024-03-08 06:54:59 +0100

    Comentado todo el ResetScene porque no se necesita y da mil errores

--- Assets/Scripts/ResetScene.cs
+++ Assets/Scripts/ResetScene.cs
@@ -5,20 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class ResetScene : MonoBehaviour
 {
-    [SerializeField] private HealthComponent health;
+    //[SerializeField] private HealthComponent health;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    //// Start is called before the first frame update
+    //void Start()
+    //{
         
-    }
+    //}
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(health._currentHp<=0)
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        }
-    }
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    if(health._currentHp<=0)
+    //    {
+    //        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    //    }
+    //}
 }
