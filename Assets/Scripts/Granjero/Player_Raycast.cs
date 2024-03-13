44619716708015096604fdc917b2ee3d93090316
JavierFueyo <157507995+JavierFueyo@users.noreply.github.com>
2024-03-13 10:06:59 +0100

    Arreglado el bug de chocarse cn las paredes (esta vez si, lo juro)
    He creado otros 4 raycast (RightUp, RightDown, LeftUp, LeftDown) para que siempre detecte las colisiones con las plataformas flotantes

--- Assets/Scripts/Granjero/Player_Raycast.cs
+++ Assets/Scripts/Granjero/Player_Raycast.cs
@@ -10,8 +10,12 @@ public class Player_Raycast : MonoBehaviour
     private GranjeroMovement _movimientoPlayer;
     Transform _myTransform;
     RaycastHit2D hitRight;
+    RaycastHit2D hitRightUp;
+    RaycastHit2D hitRightDown;
     RaycastHit2D hitDown;
     RaycastHit2D hitLeft;
+    RaycastHit2D hitLeftUp;
+    RaycastHit2D hitLeftDown;
     [SerializeField] LayerMask layerToJump;
     [SerializeField] LayerMask layerToWalls;
     public bool _allowTrampoline = false;
@@ -25,18 +29,30 @@ public class Player_Raycast : MonoBehaviour
 
     void FixedUpdate()
     {
+        Vector2 positionUp = new Vector2(transform.position.x, transform.position.y + 0.5f);
+        Vector2 positionDown = new Vector2(transform.position.x, transform.position.y - 0.5f);
         hitRight = Physics2D.Raycast(_myTransform.position, transform.right, distanceSide, layerToWalls);
-        hitDown = Physics2D.Raycast(_myTransform.position, transform.up * -1, distanceDown, layerToJump);
-        hitLeft = Physics2D.Raycast(_myTransform.position, transform.right * -1, distanceSide, layerToWalls);
+        hitRightUp = Physics2D.Raycast(positionUp, transform.right, distanceSide, layerToWalls);
+        hitRightDown = Physics2D.Raycast(positionDown, transform.right, distanceSide, layerToWalls);
 
-        if (hitRight.collider != null)
+        hitDown = Physics2D.Raycast(_myTransform.position, transform.up * -1, distanceDown, layerToJump);
+
+        hitLeft = Physics2D.Raycast(_myTransform.position, transform.right * -1, distanceSide, layerToWalls);
+        hitLeftUp = Physics2D.Raycast(positionUp, transform.right * -1, distanceSide, layerToWalls);
+        hitLeftDown = Physics2D.Raycast(positionDown, transform.right * -1, distanceSide, layerToWalls);
+
+        if (hitRight.collider != null || hitRightUp.collider != null || hitRightDown.collider != null)
         {
             Debug.DrawRay(transform.position, transform.right, Color.green);
+            Debug.DrawRay(positionUp, transform.right, Color.green);
+            Debug.DrawRay(positionDown, transform.right, Color.green);
             _movimientoPlayer.SetBoolRight(true);
         }
         else
         {
             Debug.DrawRay(transform.position, transform.right, Color.red);
+            Debug.DrawRay(positionUp, transform.right, Color.red);
+            Debug.DrawRay(positionDown, transform.right, Color.red);
             _movimientoPlayer.SetBoolRight(false);
         }
 
@@ -52,14 +68,18 @@ public class Player_Raycast : MonoBehaviour
             _movimientoPlayer.SetBoolDown(false);
             _allowTrampoline = true;
         }
-        if (hitLeft.collider != null)
+        if (hitLeft.collider != null || hitLeftUp.collider != null || hitLeftDown.collider != null)
         {
             Debug.DrawRay(transform.position, transform.right * -1, Color.green);
+            Debug.DrawRay(positionUp, transform.right * -1, Color.green);
+            Debug.DrawRay(positionDown, transform.right * -1, Color.green);
             _movimientoPlayer.SetBoolLeft(true);
         }
         else
         {
             Debug.DrawRay(transform.position, transform.right * -1, Color.red);
+            Debug.DrawRay(positionUp, transform.right * -1, Color.red);
+            Debug.DrawRay(positionDown, transform.right * -1, Color.red);
             _movimientoPlayer.SetBoolLeft(false);
         }
     }
