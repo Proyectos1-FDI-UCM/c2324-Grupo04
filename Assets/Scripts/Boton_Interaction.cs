bb206446f7c17c9c08ec68e80ee9d297a0f6acd7
JavierFueyo <157507995+JavierFueyo@users.noreply.github.com>
2024-04-12 11:53:13 +0200

    Creada la escena del nivel 2, y botón funcionall
    - Creada la escena "Nivel 2".
    - Creado el prefab "Boton".
    - Creado el script "Boton_Interaction", funciona a la perfección.
    - Creado el script "Control_ObjetosQueFuncionanPorBoton", estoy trabajando en ello

---
+++ Assets/Scripts/Boton_Interaction.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Boton_Interaction : MonoBehaviour
+{
+    public bool botonActivo = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.GetComponent<GranjeroMovement>() != null || collision.GetComponent<OvejaBalido>() != null || collision.GetComponent<BloqueMovible>())
+        {
+            //Debug.Log("Botón pulsado");
+            botonActivo = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+
+        if (collision.GetComponent<GranjeroMovement>() != null || collision.GetComponent<OvejaBalido>() != null || collision.GetComponent<BloqueMovible>())
+        {
+            //Debug.Log("Botón soltado");
+            botonActivo = false;
+        }
+    }
+}
