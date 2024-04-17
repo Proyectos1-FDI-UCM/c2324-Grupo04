2ddedb0a5b5194a9f2829c187cc383c17408ff51
RubensDieWeise <149378012+RubensDieWeise@users.noreply.github.com>
2024-04-17 10:05:09 +0200

    Pequeño tiempo de andar antes de correr
    Para hacer al personaje más fácil de controlar

--- Assets/Scripts/Granjero/GranjeroMovement.cs
+++ Assets/Scripts/Granjero/GranjeroMovement.cs
@@ -6,15 +6,15 @@ using UnityEngine.InputSystem;
 public class GranjeroMovement : MonoBehaviour
 {
     #region references
-    private GranjeroAnimationController _myAnimationController;
+    private PlayerAnimationController _myAnimationController;
     private Rigidbody2D _myRB; // Esto estaba público por alguna razón? - R
     private Player_Raycast _myRC;
     #endregion
 
     
 
-    public Vector2 movementTracker;
-    private float _maxHorizontalSpeed;
+    
+    
 
     #region parameters
     [SerializeField] private float _sheepMaxSpeed = 6;
@@ -26,11 +26,16 @@ public class GranjeroMovement : MonoBehaviour
     [SerializeField] private float _fallSpeed = 4;
     [SerializeField] private float _maxFallSpeed = 4;
     [SerializeField] private float _maxVerticalSpeed = 30;
+    [SerializeField] private float _walkTime = 0.8f;
+    [SerializeField] private float _walkSpeed = 3;
     #endregion
 
     #region variables
     private Vector2 _movementDirection;
-
+    public Vector2 _movementTracker;
+    private float _maxHorizontalSpeed;
+    private float _currentAcceleration = 0;
+    private float _walkCounter = 0;
 
     public bool choqueAbajo;
     public bool choqueIzq;
@@ -58,16 +63,17 @@ public class GranjeroMovement : MonoBehaviour
 
     public Vector2 Movement()
     {
-        return movementTracker;
+        return _movementTracker;
     }
 
     private void Start()
     {
         _maxHorizontalSpeed = _maxSpeed;
         _maxFallSpeed = _jumpForce;
-        _myAnimationController = GetComponent<GranjeroAnimationController>();
+        _myAnimationController = GetComponent<PlayerAnimationController>();
         _myRB = GetComponent<Rigidbody2D>();
         _myRC = GetComponent<Player_Raycast>();
+        OvejaSoltada();
     }
 
 
@@ -85,7 +91,7 @@ public class GranjeroMovement : MonoBehaviour
     private void OnHorizontalMovement (InputValue value) 
     {
         _movementDirection = value.Get<Vector2>(); //Este vector siempre tendrá la forma (1, 0) o (-1, 0)
-        movementTracker = _movementDirection;
+        _movementTracker = _movementDirection;
         _myAnimationController.Gira(_movementDirection.x);
         print($"Vector de la entrada: ({_movementDirection.x}, {_movementDirection.y})");
 
@@ -111,14 +117,16 @@ public class GranjeroMovement : MonoBehaviour
     {
         //Debug.Log("OvejaSoltada()");
         _maxHorizontalSpeed = _maxSpeed;
-        _maxFallSpeed = _jumpForce;
+        _maxVerticalSpeed = _jumpForce;
+        _currentAcceleration = _acceleration;
     }
 
     public void OvejaRecogida()
     {
         //Debug.Log("OvejaRecogida()");
         _maxHorizontalSpeed = _sheepMaxSpeed;
-        _maxFallSpeed = _sheepJumpForce;
+        _maxVerticalSpeed= _sheepJumpForce;
+        _currentAcceleration = _sheepAcceleration;
     }
 
     private void FixedUpdate () // ¿Hay alguna razón por la que hagáis este cálculo en el FixedUpdate()? - R
@@ -139,17 +147,60 @@ public class GranjeroMovement : MonoBehaviour
 
         // Versión con velocidad clampeada y aceleración progresiva - R
 
-        float horizontalVel = Mathf.Clamp(_myRB.velocity.x, -_maxHorizontalSpeed, _maxHorizontalSpeed);
-        float verticalVel = Mathf.Clamp(_myRB.velocity.y, -_maxFallSpeed, _maxVerticalSpeed);
-        if ((_movementDirection.x < 0 && !choqueIzq) || (_movementDirection.x > 0 && !choqueDer))
+        //float horizontalVel = Mathf.Clamp(_myRB.velocity.x, -_maxHorizontalSpeed, _maxHorizontalSpeed);
+        //float verticalVel = Mathf.Clamp(_myRB.velocity.y, -_maxFallSpeed, _maxVerticalSpeed);
+        //if ((_movementDirection.x < 0 && !choqueIzq) || (_movementDirection.x > 0 && !choqueDer))
+        //{
+        //    _myRB.velocity = _movementDirection * _maxHorizontalSpeed + Vector2.up * _myRB.velocity.y;
+        //}
+        //if (_myRB.velocity.y < -_fallSpeed)
+        //{
+        //    _myRB.velocity = new Vector2(_myRB.velocity.x, -_fallSpeed);
+        //}
+        //_myRB.velocity += _acceleration * _movementDirection;
+        //_myRB.velocity += _fallSpeed * _movementDirection;
+
+        if (_movementDirection.x < 0 || _movementDirection.x > 0)
         {
-            _myRB.velocity = _movementDirection * _maxHorizontalSpeed + Vector2.up * _myRB.velocity.y;
+            if (_walkCounter >= _walkTime)
+            {
+                _myRB.velocity = _movementDirection * _maxHorizontalSpeed + Vector2.up * _myRB.velocity.y;
+                print("Anda");
+            }
+            else
+            {
+                _myRB.velocity = _movementDirection * _walkSpeed + Vector2.up * _myRB.velocity.y;
+                _walkCounter += Time.deltaTime;
+                print("Corre");
+            }
+            print($"Velocidad horizontal: {_myRB.velocity.x}, {_maxHorizontalSpeed}");
         }
-        if (_myRB.velocity.y < -_fallSpeed)
+        else
         {
-            _myRB.velocity = new Vector2(_myRB.velocity.x, -_fallSpeed);
+            _walkCounter = 0;
         }
-        _myRB.velocity = Mathf.Clamp(_myRB.velocity.x, -_maxHorizontalSpeed, _maxHorizontalSpeed) * Vector2.right + Mathf.Clamp(_myRB.velocity.y, -_maxFallSpeed, _maxVerticalSpeed) * Vector2.up;
+        //if (_myRB.velocity.y < -_fallSpeed) Esto ya lo hace el Mathf.Clamp()
+        //{
+        //    _myRB.velocity = new Vector2(_myRB.velocity.x, -_fallSpeed);
+        //}
+        // Limitación de las velocidades a los valores deseados
+        _myRB.velocity = Mathf.Clamp(_myRB.velocity.x, -_maxHorizontalSpeed, _maxHorizontalSpeed) * Vector2.right 
+                       + Mathf.Clamp(_myRB.velocity.y, -_maxFallSpeed, _maxVerticalSpeed) * Vector2.up;
+
+
+        //if (_movementDirection != Vector2.zero || true)
+        //{
+        //    print($"velocity.x: {_myRB.velocity.x}, _currentAcceleration: {_currentAcceleration}, _movementDirection.x: {_movementDirection.x}");
+        //    _myRB.velocity = Mathf.Clamp(_myRB.velocity.x + _currentAcceleration * _movementDirection.x * Time.deltaTime, -_maxHorizontalSpeed, _maxHorizontalSpeed) * Vector2.right
+        //               + Mathf.Clamp(_myRB.velocity.y - _fallSpeed * Time.deltaTime, -_maxFallSpeed, _maxVerticalSpeed) * Vector2.up;
+        //    print($"vector de velocidad: ({_myRB.velocity.x}, {_myRB.velocity.y})");
+        //}
+        //else
+        //{
+        //    _myRB.velocity = Mathf.Clamp(_myRB.velocity.y - _fallSpeed * Time.deltaTime, -_maxFallSpeed, _maxVerticalSpeed) * Vector2.up;
+        //}
+
+
         //_myRB.velocity = horizontalVel * Vector2.right + verticalVel * Vector2.up;
         //variante 3 con aceleracion2
         /*
--- Assets/Scripts/Granjero/PlayerAnimationController.cs
+++ Assets/Scripts/Granjero/PlayerAnimationController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GranjeroAnimationController : MonoBehaviour
+public class PlayerAnimationController : MonoBehaviour
 {
     #region references
     private Transform _myTransform;
