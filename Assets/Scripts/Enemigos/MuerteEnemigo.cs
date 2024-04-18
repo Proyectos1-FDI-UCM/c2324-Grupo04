21cfeeb4db3f6c36b71deba1493cb92967ea9c7a
Rubens <149378012+RubensDieWeise@users.noreply.github.com>
2024-04-18 22:33:12 +0200

    Animación de muerte de los enemigos

--- Assets/Scripts/Enemigos/MuerteEnemigo.cs
+++ Assets/Scripts/Enemigos/MuerteEnemigo.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class MuerteEnemigo : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject _explosionPrefab;
+
     public void Die()
     {
         //Llamada a la animación de muerte
+        GameObject animation = Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
+        Destroy(animation, 1.5f);
         Destroy(gameObject);
     }
 }
