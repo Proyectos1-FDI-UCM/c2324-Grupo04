0785232ee2981bc2af50daae94273d9338d1ffd4
Bucerillo <153779274+Bucerillo@users.noreply.github.com>
2024-04-19 10:21:31 +0200

    Menu de muerte implementado en todas las escenas

--- Assets/Scripts/Granjero/MuerteJugador.cs
+++ Assets/Scripts/Granjero/MuerteJugador.cs
@@ -1,14 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class MuerteJugador : MonoBehaviour
 {
+    [SerializeField] private GameObject menuMuerte;
+    [SerializeField] private GameObject reset;
     //public GameObject canvasMuerte;
-
+    public void Start()
+    {
+        menuMuerte.SetActive(false);
+    }
     public void Die()
     {
-        GameManager.Instance.ReiniciaEscena();
+       
+        menuMuerte.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(reset);
+        Time.timeScale = 0f;
+        //GameManager.Instance.ReiniciaEscena();
         /* canvasMuerte.SetActive(true);
         Time.timeScale = 0f;
         Debug.Log("El jugador ha muerto.");
