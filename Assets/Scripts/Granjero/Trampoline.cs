a3adbdee84961ebf5c549b4063bc332e801a628e
Dpgo4 <danipont@ucm.es>
2024-05-25 22:08:17 +0200

    Issue trampolines
    arreglados algunos issues relacionados al trampolin

--- Assets/Scripts/Granjero/Trampoline.cs
+++ Assets/Scripts/Granjero/Trampoline.cs
@@ -7,16 +7,20 @@ public class Trampoline : MonoBehaviour
 
     [SerializeField] private float bounce = 40f;
 
-    // Update is called once per frame
-    private void OnCollisionEnter2D(Collision2D collision)
+
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.gameObject.GetComponent<GranjeroMovement>() && collision.gameObject.GetComponent<Player_Raycast>()._allowTrampoline)
+        if ((collision.gameObject.GetComponent<GranjeroMovement>() != null) && (collision.gameObject.GetComponent<Player_Raycast>()._allowTrampoline != null))
         {
             collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounce, ForceMode2D.Impulse);
         }
-        //if (collision.transform.CompareTag("Player"))
-        //{
-        //    collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounce, ForceMode2D.Impulse);
-        //}
     }
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.GetComponent<GranjeroMovement>() && collision.gameObject.GetComponent<Player_Raycast>()._allowTrampoline)
+    //    {
+    //        collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounce, ForceMode2D.Impulse);
+    //    }
+        
+    //}
 }
