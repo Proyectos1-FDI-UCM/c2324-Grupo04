3dbd7e99144294871784a235b43d6936808de182
Dpgo4 <danipont@ucm.es>
2024-05-26 17:12:15 +0200

    Update Bullet
    La bala ya no desaparece al chocar con el límite del propio Alien, solo desaparece si choca con el granjero o la oveja

--- Assets/Scripts/Enemigos/BulletMov.cs
+++ Assets/Scripts/Enemigos/BulletMov.cs
@@ -12,7 +12,11 @@ public class BulletMov : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        _collision = true;
+        if ((collision.gameObject.GetComponent<GranjeroMovement>() != null) || (collision.gameObject.GetComponent<MovimientoOveja>() != null))
+        {
+            _collision = true;
+        }
+        
     }
 
 
