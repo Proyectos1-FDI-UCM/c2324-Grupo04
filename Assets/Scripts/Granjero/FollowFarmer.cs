9ae8e18b695c35fdd1c00ce28c4cf51ab0a4210e
carlosmesat <159536889+carlosmesat@users.noreply.github.com>
2024-07-01 20:09:26 +0200

    Update FollowFarmer.cs

--- Assets/Scripts/Granjero/FollowFarmer.cs
+++ Assets/Scripts/Granjero/FollowFarmer.cs
@@ -5,20 +5,19 @@ using UnityEngine;
 public class FollowFarmer : MonoBehaviour
 {
     [SerializeField] private Transform farmer;
-    private Transform _mytransform;
-    private float x;
+    private Transform _transform;
     private float y = 10;
     private float z = 0;
     // Start is called before the first frame update
     void Start()
     {
-        _mytransform = transform;
+        _transform = transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-    x = farmer.position.x;
-    _mytransform.position = new Vector3 (x,y,z);
+    float x = farmer.position.x;
+    _transform.position = new Vector3 (x,y,z);
     }
 }
